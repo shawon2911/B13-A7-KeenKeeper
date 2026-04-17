2413bf6b9854a57ce646039b6dc84f7c0b1ef9a1
shawon2911 <hosenuzzaman2018@gmail.com>
2026-04-17 13:09:30 +0600

    footer part enabled

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -10,7 +10,7 @@ const MainLayout = () => {
             <Navbar />
             <ScrollTop />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 };
